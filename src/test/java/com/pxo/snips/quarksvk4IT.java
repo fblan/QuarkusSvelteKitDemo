38e0eafa67a92161a8681eb22f799cea8f3f93d7
fblan <frederic.blanc@praxedo.com>
2024-10-16 19:52:10 +0200

    tester c'est douter

--- src/test/java/com/pxo/snips/quarksvk4IT.java
+++
@@ -1,8 +0,0 @@
-package com.pxo.snips;
-
-import io.quarkus.test.junit.QuarkusIntegrationTest;
-
-@QuarkusIntegrationTest
-class quarksvk4IT extends quarksvk4Test {
-    // Execute the same tests but in packaged mode.
-}
